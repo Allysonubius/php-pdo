b9b5925ece032c8edb788d039e1d83ecb6133692
Allysom Oliveira <allysomoliveira7@gmail.com>
2020-06-10 16:25:15 -0300

    Iniciado estudo para trabalhar com banco de dados PDO com PHP.

---
+++ src/connect.php
@@ -0,0 +1,18 @@
+<?php
+/**Created by Visual Studio Code
+ * Name: Allyson
+ * Date: 10/05/2020
+ * PHP Version 7.4
+*/
+declare(strict_types=1);
+//connection
+$pdo = null;
+//Metodo try, catch
+try{
+    $pdo = new PDO('mysql:host=mysql ; dbname=exemplo' , 'root' , '123456');//execução do banco de dados
+}catch(Exception $e){//Excessão
+    echo $e -> getMessage();
+    die();//fim 
+}
+return $pdo;
+?>
